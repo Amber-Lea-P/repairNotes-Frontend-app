8ab574463d73225670e7530247801e84b2ee221e
Amber-Lea-P <amber.pontinen@gmail.com>
2023-04-10 11:04:56 +0300

    updates to the notes and user sections

--- src/App.js
+++ src/App.js
@@ -6,6 +6,12 @@ import DashLayout from './components/DashLayout.js';
 import Welcome from './features/auth/Welcome.js';
 import NotesList from './features/notes/NotesList.js';
 import UsersList from './features/users/UsersList.js';
+import EditUser from './features/users/EditUser.js';
+import NewUserForm from './features/users/NewUserForm.js';
+import EditNote from './features/notes/EditNote.js';
+import NewNote from './features/notes/NewNote.js';
+import Prefetch from './features/auth/Prefetch.js';
+
 
 function App() {
   return (
@@ -14,20 +20,25 @@ function App() {
           <Route index element={<Public />} />
           <Route path="login" element={<Login />} />
           
-          <Route path="dash" element={<DashLayout />}>
+          <Route element={<Prefetch />}>
+              <Route path="dash" element={<DashLayout />}>
 
-          <Route index element={<Welcome />} />
+              <Route index element={<Welcome />} />
 
-          <Route path="notes">
-              <Route index element={<NotesList />} />
+              <Route path="users">
+                  <Route index element={<UsersList />} />
+                  <Route path=":id" element={<EditUser />} />
+                  <Route path="new" element={<NewUserForm />} />
+                </Route>
+
+                <Route path="notes">
+                  <Route index element={<NotesList />} />
+                  <Route path=":id" element={<EditNote />} />
+                  <Route path="new" element={<NewNote />} />
+                </Route>
+
+              </Route>{/* End Dash */}
           </Route>
-
-          <Route path="users">
-              <Route index element={<UsersList />} />
-          </Route>
-
-          </Route>{/* End Dash */}
-
       </Route>
     </Routes>
   );
